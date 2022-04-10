3db3982eb33314e2bd46f9442537a9248965728c
James R <james.rodriguez1215@gmail.com>
2022-04-10 18:49:07 -0400

    Added pic of zenitsu rug w Adam

---
+++ android/app/src/main/kotlin/com/example/shenzrugs/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.shenzrugs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_insta/flutter_insta.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData.light().copyWith(
+            colorScheme: ColorScheme.light()
+                .copyWith(primary: Color.fromARGB(255, 91, 171, 237))),
+        home: const MyHomePage());
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  //FlutterInsta flutterInsta = FlutterInsta();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    // await flutterInsta.getProfileData("shenzrugs");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Shenzrugs'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 250,
+              width: 250,
+              child: Image.asset("assets/zenitsu.jpg"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
